1297ea9999df36eb179d0513dc408c52d5dfd1c2
sshpros <kballard@sshpros.com>
2026-04-20 13:20:51 -0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+return (
+  <div className="container">
+    <div className="header" style={{ marginTop: 80 }}>
+      <h1>Shield Low Voltage</h1>
+      <p>Please open the estimate link sent to you via SMS or email.</p>
+    </div>
+  </div>
+);
+}
